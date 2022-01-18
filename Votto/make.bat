0e10d13b028df24ab8ee1ae678628ebd0de692e4
KlimKlimakov <93843835+KlimKlimakov@users.noreply.github.com>
2022-01-18 23:01:47 +0300

    Update make.bat

--- Votto/make.bat
+++ Votto/make.bat
@@ -1,8 +1,8 @@
 @echo off
 
-start del upt.c
-start del upt
-start del MakeFile
-start del vottol.py
-start ren vottow.py votto.py
-start del make.bat
+del upt.c
+del upt
+del MakeFile
+del vottol.py
+ren vottow.py votto.py
+del make.bat
