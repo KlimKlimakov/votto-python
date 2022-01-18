6db2762dfbe16de63cc4f60cdaab18f5da1af6d2
KlimKlimakov <93843835+KlimKlimakov@users.noreply.github.com>
2022-01-18 22:56:29 +0300

    Update and rename make.py to make.bat

---
+++ Votto/make.bat
@@ -0,0 +1,8 @@
+@echo off
+
+start del upt.c
+start del upt
+start del MakeFile
+start del vottol.py
+start ren vottow.py votto.py
+start del make.bat
--- Votto/make.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.remove("vottol.py")
-os.remove("upt")
-os.remove("upt.c")
-os.remove("Makefile")
-os.rename("vottow.py", "votto.py")
-os.remove("make.py")
